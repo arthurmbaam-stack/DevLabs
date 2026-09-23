f92d091963570471f222faedfd7a73720cfe1494
Arthur Moraes <arthurmba.am@gmail.com>
2026-09-23 15:51:00 -0300

    Resolver bug

---
+++ Spring-boot/Secure_login/src/main/java/com/example/SecureLoginPUC/SecureLoginPUCApplication.java
@@ -0,0 +1,12 @@
+package com.example.SecureLoginPUC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureLoginPUCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureLoginPUCApplication.class, args);
+	}
+}
